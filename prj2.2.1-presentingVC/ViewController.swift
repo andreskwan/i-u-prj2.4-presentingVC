fe9a69ad1d36e65c95731144af96810cdb8d36f3
ANDRES KWAN <andreskwan@gmail.com>
2015-04-18 13:34:28 -0500

    Initial Commit

---
+++ prj2.2.1-presentingVC/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  prj2.2.1-presentingVC
+//
+//  Created by Andres Kwan on 4/18/15.
+//  Copyright (c) 2015 Kwan Castle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
