794294e200b1bf320f42f5f3a43a53f98a3f526d
ANDRES KWAN <andreskwan@gmail.com>
2015-04-19 11:43:18 -0500

    3 VC presented, UIImagePickerVC, UIActivityVC, UIAlertVC

--- prj2.2.1-presentingVC/ViewController.swift
+++ prj2.2.1-presentingVC/ViewController.swift
@@ -20,6 +20,29 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func experiment(sender: UIButton) {
+        let nextController = UIImagePickerController()
+        self.presentViewController(nextController, animated: true, completion: nil)
+    }
+    
+    @IBAction func experimentWithActivityVC(sender: UIButton){
+        let image = UIImage()
+        let controller = UIActivityViewController(activityItems: [image], applicationActivities: nil)
+        self.presentViewController(controller, animated: true, completion: nil)
+    }
 
+    @IBAction func experimentWithAlertVC(sender: UIButton) {
+        let controller = UIAlertController()
+        controller.title = "Test alert"
+        controller.message = "Alert message"
+        
+        let okAction = UIAlertAction(title: "ok", style: UIAlertActionStyle.Default){
+            action in
+            self.dismissViewControllerAnimated(true, completion: nil)
+        }
+        controller.addAction(okAction)
+        
+        self.presentViewController(controller, animated: true, completion: nil)
+    }
 }
 
